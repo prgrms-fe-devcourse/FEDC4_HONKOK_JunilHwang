334b876c4fd6c8552bdfb19534b78d7c3175432c
jgjgill <dbdltm22@naver.com>
2023-09-05 00:38:42 +0900

    feat: eslint 관련 작업 @rushstack/eslint-config 추가 @typescript-eslint/eslint-plugin 삭제 eslintrc.cjs 설정 변경

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,27 @@
+require('@rushstack/eslint-config/patch/modern-module-resolution');
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    '@rushstack/eslint-config/profile/web-app',
+    '@rushstack/eslint-config/mixins/react'
   ],
+  parserOptions: { tsconfigRootDir: __dirname },
+  settings: {
+    react: {
+      version: '18.2.0'
+    }
+  },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+      { allowConstantExport: true }
+    ]
+  }
+};
