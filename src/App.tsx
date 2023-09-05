5e6b59ef8922b46d0519153a63cf8b284ab6e9b3
jgjgill <dbdltm22@naver.com>
2023-09-05 22:44:17 +0900

    feat: 라우팅 관련 작업 react-router-dom 설치 기본 라우팅 파일 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { HomePage } from './pages';
+import { PATH } from './constants';
+
+const router = createBrowserRouter([
+  {
+    path: PATH.HOME,
+    element: <HomePage />,
+    errorElement: <div>Error Page</div>
+  }
+]);
+
 const App = () => {
-  return <div />;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 };
 
 export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <div>Home page</div>;
+};
+
+export default HomePage;
