dd1b74fcde7fbe657b0d1e24adb332472a2ffeff
backward99 <wjeeho@naver.com>
2023-09-06 02:24:44 +0900

    feat: react-query 기본 설정 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient: QueryClient = new QueryClient();
+
 const App = () => {
-  return <div></div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div />
+      <ReactQueryDevtools initialIsOpen />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
