b5ffd8dff51f2ad52aed71d7f764388d78d4bed4
jgjgill <79239852+jgjgill@users.noreply.github.com>
2023-09-05 16:14:35 +0900

    refactor: eslint 관련 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,9 @@
-require('@rushstack/eslint-config/patch/modern-module-resolution');
-
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     '@rushstack/eslint-config/profile/web-app',
     '@rushstack/eslint-config/mixins/react'
