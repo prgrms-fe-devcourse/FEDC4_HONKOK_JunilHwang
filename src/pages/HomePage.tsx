63fcb5271052188f3c99ed02f6746dcd01517579
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-09-06 18:28:22 +0900

    feat: 채널 생성 기능 추가
    - axios 인터셉터 구현
    - 로컬스토리지에 토큰 값 저장

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,27 @@
 import { useMutation } from '@tanstack/react-query';
-import userService from '../services/userService';
+import { channelService, userService } from '../services';
 
 const HomePage = () => {
-  const mutation = useMutation(userService.login);
+  const userMutation = useMutation(userService.login, {
+    onSuccess({ data }) {
+      window.localStorage.setItem('token', data.token);
+    }
+  });
+
+  const channelMutation = useMutation(channelService.create);
 
   const handleLogin = () => {
-    mutation.mutate({
+    userMutation.mutate({
       email: 'admin@programmers.co.kr',
       password: 'programmers'
+    });
+  };
+
+  const handleCreateChannel = () => {
+    channelMutation.mutate({
+      authRequired: true,
+      description: '테스트 채널입니다.',
+      name: '테스트 채널'
     });
   };
 
@@ -15,6 +29,7 @@ const HomePage = () => {
     <div>
       <h1>Home page</h1>
       <button onClick={handleLogin}>로그인</button>
+      <button onClick={handleCreateChannel}>채널 생성</button>
     </div>
   );
 };
