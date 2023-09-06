ffbfd5732a015c3e46dac4193272da170a35ed80
jgjgill <dbdltm22@naver.com>
2023-09-06 22:23:19 +0900

    feat: 회원가입 api 관련 로직 구성

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,25 @@
 import { useMutation } from '@tanstack/react-query';
 import { channelService, userService } from '../services';
+import { useState } from 'react';
 
 const HomePage = () => {
+  const [email, setEmail] = useState('');
+  const [fullName, setFullName] = useState('');
+  const [password, setPassword] = useState('');
+
   const userMutation = useMutation(userService.login, {
     onSuccess({ data }) {
       window.localStorage.setItem('token', data.token);
     }
   });
 
-  const channelMutation = useMutation(channelService.create);
+  const channelMutation = useMutation({ mutationFn: channelService.create });
+
+  const signupMutation = useMutation(userService.signup, {
+    onSuccess({ data }) {
+      console.log(data);
+    }
+  });
 
   const handleLogin = () => {
     userMutation.mutate({
@@ -25,11 +36,35 @@ const HomePage = () => {
     });
   };
 
+  const handleSignup = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    signupMutation.mutate({ email, fullName, password });
+  };
+
   return (
     <div>
       <h1>Home page</h1>
       <button onClick={handleLogin}>로그인</button>
       <button onClick={handleCreateChannel}>채널 생성</button>
+
+      <div>
+        <h2>임시 회원가입</h2>
+        <form onSubmit={handleSignup}>
+          <input
+            type="email"
+            onChange={(e) => setEmail(e.currentTarget.value)}
+          />
+          <input
+            type="fullName"
+            onChange={(e) => setFullName(e.currentTarget.value)}
+          />
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.currentTarget.value)}
+          />
+          <button>회원가입 버튼</button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,11 +1,23 @@
 import { apiClient } from '../api';
 
+type Login = {
+  email: string;
+  password: string;
+};
+
+type Signup = {
+  email: string;
+  fullName: string;
+  password: string;
+};
+
 const userService = {
-  async login({ email, password }: { email: string; password: string }) {
-    return await apiClient.post('/login', {
-      email,
-      password
-    });
+  async login({ email, password }: Login) {
+    return await apiClient.post('/login', { email, password });
+  },
+
+  async signup({ email, fullName, password }: Signup) {
+    return await apiClient.post('/signup', { email, fullName, password });
   }
 };
 
