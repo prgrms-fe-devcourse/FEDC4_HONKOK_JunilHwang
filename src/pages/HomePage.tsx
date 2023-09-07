73c25051996217d8d426185bc9856aedd33942ad
jgjgill <79239852+jgjgill@users.noreply.github.com>
2023-09-07 14:13:56 +0900

    회원가입 api 관련 작업 (#32)
    * feat: 개발 관련 env 파일 추가
    
    * feat: react-query 관련 플러그인 추가
    
    * feat: 회원가입 api 관련 로직 구성
    
    * refactor: useForm 훅 추가
    
    * refator: 임시 로그인 관련 코드 변경
    
    * refactor: login 이름 변경

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,20 +1,31 @@
 import { useMutation } from '@tanstack/react-query';
 import { channelService, userService } from '../services';
+import useForm from '../hooks/useForm';
 
 const HomePage = () => {
-  const userMutation = useMutation(userService.login, {
+  const [loginEmail, handleChangeLoginEmail] = useForm();
+  const [loginPassword, handleChangeLoginPassword] = useForm();
+  const [email, handleChangeEmail] = useForm();
+  const [fullName, handleFullName] = useForm();
+  const [password, handleChangePassword] = useForm();
+
+  const userMutation = useMutation(userService.signin, {
     onSuccess({ data }) {
       window.localStorage.setItem('token', data.token);
     }
   });
 
-  const channelMutation = useMutation(channelService.create);
+  const channelMutation = useMutation({ mutationFn: channelService.create });
 
-  const handleLogin = () => {
-    userMutation.mutate({
-      email: 'admin@programmers.co.kr',
-      password: 'programmers'
-    });
+  const signupMutation = useMutation(userService.signup, {
+    onSuccess({ data }) {
+      console.log(data);
+    }
+  });
+
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    userMutation.mutate({ email: loginEmail, password: loginPassword });
   };
 
   const handleCreateChannel = () => {
@@ -25,11 +36,35 @@ const HomePage = () => {
     });
   };
 
+  const handleSignup = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    signupMutation.mutate({ email, fullName, password });
+  };
+
   return (
     <div>
       <h1>Home page</h1>
-      <button onClick={handleLogin}>로그인</button>
+
+      <div>
+        <h2>임시 로그인</h2>
+        <form onSubmit={handleLogin}>
+          <input type="email" onChange={handleChangeLoginEmail} />
+          <input type="password" onChange={handleChangeLoginPassword} />
+          <button>로그인</button>
+        </form>
+      </div>
+
       <button onClick={handleCreateChannel}>채널 생성</button>
+
+      <div>
+        <h2>임시 회원가입</h2>
+        <form onSubmit={handleSignup}>
+          <input type="email" onChange={handleChangeEmail} />
+          <input type="fullName" onChange={handleFullName} />
+          <input type="password" onChange={handleChangePassword} />
+          <button>회원가입 버튼</button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,11 +1,23 @@
 import { apiClient } from '../api';
 
+type Signin = {
+  email: string;
+  password: string;
+};
+
+type Signup = {
+  email: string;
+  fullName: string;
+  password: string;
+};
+
 const userService = {
-  async login({ email, password }: { email: string; password: string }) {
-    return await apiClient.post('/login', {
-      email,
-      password
-    });
+  async signin({ email, password }: Signin) {
+    return await apiClient.post('/login', { email, password });
+  },
+
+  async signup({ email, fullName, password }: Signup) {
+    return await apiClient.post('/signup', { email, fullName, password });
   }
 };
 
