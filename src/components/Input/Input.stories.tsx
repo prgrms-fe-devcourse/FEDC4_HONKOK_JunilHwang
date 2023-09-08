376acc305d6bc66e42ff544952285ea7626cb21e
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-09-08 15:26:10 +0900

    refactor: Input 컴포넌트 리팩토링
    - 기본 Input 속성 prop 제거
    - className prop 추가
    - tailwind 속성 띄어쓰기 수정

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -2,7 +2,7 @@ import { Meta, StoryObj } from '@storybook/react';
 import Input from './Input';
 
 const meta: Meta<typeof Input> = {
-  title: 'Form/Example/Input',
+  title: 'Component/Input',
   component: Input,
   argTypes: {
     onChange: { action: 'onChange' }
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,17 +1,16 @@
 import { InputHTMLAttributes } from 'react';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  onChange?: () => void;
-  type?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
 }
 
-const Input = ({ onChange, type, ...props }: InputProps) => {
+const Input = ({ onChange, type, className, ...props }: InputProps) => {
   return (
     <input
-      {...props}
-      type={type}
       onChange={onChange}
-      className="h-6  border bg-white p-4 "
+      className={`h-6 border bg-white p-4 ${className}`}
+      {...props}
     />
   );
 };
