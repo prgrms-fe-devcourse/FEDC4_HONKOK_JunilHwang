49d185a0c7c14fca252b16448e3112c470db2a7b
backward99 <wjeeho@naver.com>
2023-09-10 22:02:13 +0900

    feat: Input 컴포넌트로 textarea 사용

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,18 +1,34 @@
-import { InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes, TextareaHTMLAttributes } from 'react';
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+interface BaseProps {
   className?: string;
+  component?: 'input' | 'textarea';
 }
+interface InputProps extends BaseProps, InputHTMLAttributes<HTMLInputElement> {}
+interface TextareaProps
+  extends BaseProps,
+    TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
-const Input = ({ onChange, type, className, ...props }: InputProps) => {
-  return (
-    <input
-      onChange={onChange}
-      className={`h-6 border bg-white p-4 ${className}`}
-      {...props}
-    />
-  );
+const Input = ({
+  component = 'input',
+  className,
+  ...props
+}: InputProps | TextareaProps) => {
+  if (component === 'input') {
+    return (
+      <input
+        className={`h-6 border bg-white p-4 ${className}`}
+        {...(props as InputHTMLAttributes<HTMLInputElement>)}
+      />
+    );
+  } else if (component === 'textarea') {
+    return (
+      <textarea
+        className={` h-[40vh] w-[100%] resize-none rounded-lg border bg-white p-4  ${className}`}
+        {...(props as TextareaHTMLAttributes<HTMLTextAreaElement>)}
+      />
+    );
+  }
 };
 
 export default Input;
