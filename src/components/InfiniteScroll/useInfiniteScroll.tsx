a4bba9f8c8aabfc8c440fb4b98568b94dc19bded
jkea1 <jinkyng7890@gmail.com>
2023-09-13 01:58:23 +0900

    refactor: useInfiniteScroll 수정

--- src/components/InfiniteScroll/useInfiniteScroll.tsx
+++ src/components/InfiniteScroll/useInfiniteScroll.tsx
@@ -1,42 +1,22 @@
-import { useEffect } from 'react'
-import { useInfiniteQuery } from '@tanstack/react-query'
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
 
 const useInfiniteScroll = ({ fetchData }: any) => {
   const {
     data = { pages: [], pageParams: undefined },
     hasNextPage,
-    fetchNextPage,
+    fetchNextPage
   } = useInfiniteQuery(['posts'], fetchData, {
     getNextPageParam: (_: any, allPages: string | any[]) => {
-      return allPages.length
-    },
-  })
-
-  useEffect(() => {
-    let fetching = false
-    const onScroll = async () => {
-      const { scrollHeight, scrollTop, clientHeight } = document.documentElement
-      if (!fetching && scrollHeight - scrollTop <= clientHeight * 1.5) {
-        fetching = true
-        if (hasNextPage) {
-          await fetchNextPage()
-        }
-        fetching = false
-      }
+      return allPages.length;
     }
-
-    document.addEventListener('scroll', onScroll)
-
-    return () => {
-      document.removeEventListener('scroll', onScroll)
-    }
-  }, [hasNextPage, fetchNextPage])
+  });
 
   return {
     data: data.pages,
     hasNextPage,
-    fetchNextPage,
-  }
-}
+    fetchNextPage
+  };
+};
 
-export default useInfiniteScroll
+export default useInfiniteScroll;
