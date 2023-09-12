31b1ca6ff8b2f04683f3cb2160251c7cc9f1d142
HongGunWoo <foxrain.gg@gmail.com>
2023-09-13 03:27:59 +0900

    style: Profile 페이지 디자인 추가

--- src/pages/ProfilePage/ProfileHeader.tsx
+++ src/pages/ProfilePage/ProfileHeader.tsx
@@ -1,8 +1,12 @@
 import { useMutation } from '@tanstack/react-query';
 import { useRef } from 'react';
 import { snsApiClient } from '~/api';
-import { Avatar } from '~/components';
+import { Avatar, Button } from '~/components';
 import { useUser } from '~/hooks';
+
+const InfoBox = ({ children }: { children: React.ReactNode }) => {
+  return <div className="flex flex-col items-center">{children}</div>;
+};
 
 const ProfileHeader = () => {
   const { user, updateUser } = useUser();
@@ -37,31 +41,41 @@ const ProfileHeader = () => {
   };
 
   return (
-    <div className="flex">
-      {mutation.isLoading ? (
-        <div>로딩중...</div>
-      ) : (
-        <div onClick={handleChooseFile}>
-          <input
-            type="file"
-            className="hidden"
-            ref={inputRef}
-            onChange={handleFileChange}
-          />
-          <Avatar src={image} size="large" />
-        </div>
-      )}
-      <div>
-        <div>게시물 수</div>
-        <div>{posts.length}</div>
+    <div className="py-6">
+      <div className="grid grid-cols-4 items-center justify-items-center">
+        {mutation.isLoading ? (
+          <div>로딩중...</div>
+        ) : (
+          <div onClick={handleChooseFile}>
+            <input
+              type="file"
+              className="hidden"
+              ref={inputRef}
+              onChange={handleFileChange}
+            />
+            <Avatar src={image} size="large" />
+          </div>
+        )}
+        <InfoBox>
+          <div>게시물 수</div>
+          <div>{posts.length}</div>
+        </InfoBox>
+        <InfoBox>
+          <div>팔로워</div>
+          <div>{followers.length}</div>
+        </InfoBox>
+        <InfoBox>
+          <div>팔로잉</div>
+          <div>{following.length}</div>
+        </InfoBox>
       </div>
-      <div>
-        <div>팔로워</div>
-        <div>{followers.length}</div>
-      </div>
-      <div>
-        <div>팔로잉</div>
-        <div>{following.length}</div>
+      <div className="mt-4 grid grid-cols-2 gap-7 px-4">
+        <Button className="rounded-xl border-none py-2 text-white">
+          내 정보 변경
+        </Button>
+        <Button className="border-main-base rounded-xl bg-white text-black">
+          프로필 공유
+        </Button>
       </div>
     </div>
   );
