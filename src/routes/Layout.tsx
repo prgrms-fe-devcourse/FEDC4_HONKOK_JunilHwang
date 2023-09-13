2ea99dd12cd16e0d59d73bdad99b3a744c6eeac1
jgjgill <dbdltm22@naver.com>
2023-09-13 16:42:52 +0900

    style: 회원가입 페이지 디자인 작업

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <div className="mx-auto w-[24.375rem]">
+    <div className="mx-auto w-[24.375rem] font-OAGothic">
       <Outlet />
     </div>
   );
