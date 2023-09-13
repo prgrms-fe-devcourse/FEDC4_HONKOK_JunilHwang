0bfa5d717298022969abb4197d4cd5507d4f47b3
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-09-13 17:36:26 +0900

    style: 메인 페이지 디자인

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,4 @@
-export { default as HomePage } from './HomePage';
+export { HomePage } from './HomePage';
 export { default as SignupPage } from './SignupPage';
 export { default as PostPage } from './PostPage';
 export { default as PostEditPage } from './PostEditPage';
