6ba916761ce4549e2752be276ef979425bdcdd2c
jgjgill <79239852+jgjgill@users.noreply.github.com>
2023-09-13 14:31:57 +0900

    feat: 라우팅 관련 작업
    * feat: 라우팅 기본 분류 작업
    
    * feat: 기본 레이아웃 설정

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,8 @@
 export { default as HomePage } from './HomePage';
+export { default as SignupPage } from './SignupPage';
+export { default as PostPage } from './PostPage';
+export { default as PostEditPage } from './PostEditPage';
+export { default as ProfilePage } from './ProfilePage';
+export { default as FollowPage } from './FollowPage';
+export { default as ChatPage } from './ChatPage';
+export { default as ChatDetailPage } from './ChatDetailPage';
--- src/routes/constants.ts
+++ src/routes/constants.ts
@@ -1,3 +1,10 @@
 export const PATH = {
-  HOME: '/'
+  HOME: '/',
+  SIGNUP: '/signup',
+  POSTS: '/posts/:postId',
+  POST_EDIT: '/post-edit',
+  PROFILE: '/profile/:userId',
+  FOLLOW: '/follow',
+  CHAT: '/chat',
+  CHAT_DETAIL: '/chat/:chatId'
 };
