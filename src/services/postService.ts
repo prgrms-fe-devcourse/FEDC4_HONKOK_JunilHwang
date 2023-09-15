d7b05c0dd1d2115dc1e8fe4b477a1cab3cabf136
backward99 <86753969+backward99@users.noreply.github.com>
2023-09-15 17:27:19 +0900

    feat: 포스트 목록 받아오기 api 생성 (#77)
    * feat: 특정 채널의 포스트 목록 불러오기
    
    * chore: api 테스트 하기
    
    * style: 회원가입 테스트 부분 스타일 수정
    
    ---------
    
    Co-authored-by: HongGunWoo <foxrain.gg@gmail.com>

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -17,6 +17,12 @@ interface EditPost {
   channelId: string;
 }
 
+interface GetPosts {
+  channelId: string;
+  limit?: number;
+  offset?: number;
+}
+
 const createPost = async ({ title, content, image, channelId }: CreatePost) => {
   const customPost = JSON.stringify({ title, content });
 
@@ -62,6 +68,15 @@ const unlikePost = async (id: string) => {
   return await snsApiClient.delete('/likes/delete', { data: { id } });
 };
 
+const getPosts = async ({ channelId, limit, offset }: GetPosts) => {
+  if (!channelId) {
+    return;
+  }
+  return await snsApiClient.get(`/posts/channel/${channelId}`, {
+    params: { limit, offset }
+  });
+};
+
 export const useCreatePost = () => {
   return useMutation({ mutationFn: createPost });
 };
@@ -87,4 +102,12 @@ export const useLikePost = () => {
 
 export const useUnLikePost = () => {
   return useMutation({ mutationFn: unlikePost });
+};
+
+export const useGetPosts = ({ channelId, limit = 5, offset = 0 }: GetPosts) => {
+  return useQuery({
+    queryKey: ['Posts', channelId, limit, offset],
+    queryFn: () => getPosts({ channelId, limit, offset }),
+    retry: false
+  });
 };
