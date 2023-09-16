108652cc2041747028b5793a9d39a02da7edd7d5
backward99 <wjeeho@naver.com>
2023-09-16 23:49:39 +0900

    refactor: 스토리 파일 라우터 적용

--- src/components/common/Header/Header.stories.tsx
+++ src/components/common/Header/Header.stories.tsx
@@ -1,8 +1,10 @@
 import { Meta, StoryObj } from '@storybook/react';
+import { Route, Routes } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import { Header } from '.';
 
 const meta: Meta<typeof Header> = {
-  title: 'Component/Header',
+  title: 'Component/common/Header',
   component: Header,
   argTypes: {
     notifications: { control: 'inline-radio', options: [0, 1, 999] }
@@ -19,8 +21,17 @@ export const ExampleHeader: Story = {
     notifications: 0
   },
   render: (args) => (
-    <Header main={true} menu={true} notifications={2} {...args}>
-      글 작성하기
-    </Header>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="*"
+          element={
+            <Header main={true} menu={true} notifications={2} {...args}>
+              글 작성하기
+            </Header>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   )
 };
