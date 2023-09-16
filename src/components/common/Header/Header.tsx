08d2f67921f2992100855e34335f6c720e6c80bf
backward99 <wjeeho@naver.com>
2023-09-16 23:16:52 +0900

    feat: 아이콘 클릭시 이벤트 추가

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import { BellIcon, HomeIcon, LeftArrowIcon, SearchIcon } from '~/assets';
 import { Badge } from '~/components/common';
 
@@ -13,18 +14,37 @@ const Header = ({
   menu = true,
   notification = 1
 }: React.PropsWithChildren<HeaderProps>) => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
+  const handleGoToPage = (path: 'search' | 'notification') => {
+    navigate(path);
+  };
+
   return (
     <section className="relative flex h-28 w-full justify-center bg-main-lighten text-white">
       <h1 className="mt-14 inline-block text-xl font-bold">{children}</h1>
       {main ? (
-        <LeftArrowIcon className="absolute left-6 top-14 h-6 w-6 fill-white" />
+        <LeftArrowIcon
+          className="absolute left-6 top-14 h-6 w-6 fill-white"
+          onClick={handleGoBack}
+        />
       ) : (
         <HomeIcon className="absolute left-6 top-14 h-6 w-6 stroke-white" />
       )}
       {menu && (
         <>
-          <SearchIcon className="absolute right-16 top-14 h-6 w-6 stroke-white" />
-          <BellIcon className="absolute right-6 top-14 h-6 w-6 stroke-white" />
+          <SearchIcon
+            className="absolute right-16 top-14 h-6 w-6 stroke-white"
+            onClick={() => handleGoToPage('search')}
+          />
+          <BellIcon
+            className="absolute right-6 top-14 h-6 w-6 stroke-white"
+            onClick={() => handleGoToPage('notification')}
+          />
           {notification ? (
             <Badge className="absolute right-6 top-12 flex translate-x-1/4 translate-y-1/4 items-center justify-center border-none text-[10px] text-white cs:bg-active-base cs:px-1 cs:py-0">
               {notification}
