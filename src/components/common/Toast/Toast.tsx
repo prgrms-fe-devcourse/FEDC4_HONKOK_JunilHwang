5f6029bb18accc66a5e2e0dc307acf53fc200862
jgjgill <dbdltm22@naver.com>
2023-09-18 01:52:39 +0900

    style: 임시 스타일 추가

--- src/components/common/Toast/Toast.tsx
+++ src/components/common/Toast/Toast.tsx
@@ -9,9 +9,12 @@ const Toast = ({ id, children }: ToastProps) => {
   const { removeToast } = useToast();
 
   return (
-    <div id={id}>
+    <div
+      id={id}
+      className="absolute right-0 flex flex-col rounded-[0.625rem] border-[1.5px] px-12 py-2"
+    >
       {children}
-      <button onClick={() => removeToast({ id })}>삭제!</button>
+      <button onClick={() => removeToast({ id })}>삭제</button>
     </div>
   );
 };
