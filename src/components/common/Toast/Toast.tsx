7fa4e1ffbbd6c916c3081c1bfb9ae60d4f1650ed
jgjgill <dbdltm22@naver.com>
2023-09-18 02:09:01 +0900

    style: 토스트 스타일 수정

--- src/components/common/Toast/Toast.tsx
+++ src/components/common/Toast/Toast.tsx
@@ -11,10 +11,12 @@ const Toast = ({ id, children }: ToastProps) => {
   return (
     <div
       id={id}
-      className="absolute right-0 flex flex-col rounded-[0.625rem] border-[1.5px] px-12 py-2"
+      className="flex flex-col rounded-[0.625rem] border-[1.5px] px-12 py-2"
     >
-      {children}
-      <button onClick={() => removeToast({ id })}>삭제</button>
+      <div>
+        {children}
+        <button onClick={() => removeToast({ id })}>삭제</button>
+      </div>
     </div>
   );
 };
