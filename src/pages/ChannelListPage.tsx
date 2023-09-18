8faa4fd95d42e302ff7a5605285df62e700559f5
HongGunWoo <foxrain.gg@gmail.com>
2023-09-19 00:51:28 +0900

    fix: 잘못된 경로 수정

--- src/pages/ChannelListPage.tsx
+++ src/pages/ChannelListPage.tsx
@@ -1,5 +1,5 @@
-import { List, ListItem } from 'components/domain';
 import { useNavigate } from 'react-router-dom';
+import { List, ListItem } from '~/components/domain';
 import { useGetChannels } from '~/services';
 import { Channel } from '~/types/model';
 
