6db3d43635cbfe3fc48400867d5a1294ab55c584
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-09-18 16:25:46 +0900

    feat: 홈 링크 기능 추가

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,21 +1,18 @@
+import { Link } from 'react-router-dom';
 import Warning from './Warning';
 
 const NotFoundPage = () => {
-  const handleClick = () => {
-    console.log('홈으로 이동');
-  };
-
   return (
     <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center">
       <Warning />
       <h1 className="mt-8 text-2xl">404 ERROR</h1>
       <p className="mt-2 text-gray-400">페이지를 찾을 수 없습니다.</p>
-      <button
-        className="mt-16 h-12 w-40 rounded-[0.625rem] border-none bg-main-base font-normal text-white"
-        onClick={handleClick}
+      <Link
+        to={'/'}
+        className="mt-16 flex h-12 w-40 items-center justify-center rounded-[0.625rem] border-none bg-main-base text-white"
       >
         홈으로
-      </button>
+      </Link>
     </div>
   );
 };
