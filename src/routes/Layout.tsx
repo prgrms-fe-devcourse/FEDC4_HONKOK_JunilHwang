344773bb2f841b5f263bfcbb0e01c64ed3cc1960
backward99 <wjeeho@naver.com>
2023-09-18 21:53:02 +0900

    refactor: 헤더 레이아웃 파일 제거

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -4,7 +4,6 @@ import LayoutWithHeader from './LayoutWithHeader';
 const Layout = () => {
   return (
     <div className="mx-auto max-w-[767px] font-OAGothic">
-      <LayoutWithHeader />
       <Outlet />
     </div>
   );
