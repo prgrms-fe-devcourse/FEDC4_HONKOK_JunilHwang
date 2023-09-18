5713cd1838bccc1f6bba0681f588eb5e5337582a
backward99 <86753969+backward99@users.noreply.github.com>
2023-09-19 00:08:05 +0900

    feat: Notification 목록 받아오기 api 관련 작업 (#144)
    * feat: 알림 목록 받아오기
    
    * chore: 주석 제거

---
+++ src/services/notificationService.ts
@@ -0,0 +1,21 @@
+import { useQuery } from '@tanstack/react-query';
+import { snsApiClient } from '~/api';
+
+const notificationKeys = {
+  all: ['notifications'] as const
+};
+
+const getNotifications = async () => {
+  const response = await snsApiClient.get('/notifications');
+
+  return response.data;
+};
+
+const useGetNotifications = () => {
+  return useQuery({
+    queryKey: notificationKeys.all,
+    queryFn: getNotifications
+  });
+};
+
+export { useGetNotifications };
