4b2be104a798ca776a3c81f7327a1ad59f5a1a0c
backward99 <wjeeho@naver.com>
2023-09-18 14:30:00 +0900

    refactor: 변수명 수정, 변수 기본값 추가

--- src/pages/ChannelListPage.tsx
+++ src/pages/ChannelListPage.tsx
@@ -4,7 +4,7 @@ import { useGetChannels } from '~/services';
 import { Channel } from '~/types/model';
 
 const ChannelListPage = () => {
-  const { data: channelLsit } = useGetChannels();
+  const { data: channels = [] } = useGetChannels();
   const navigate = useNavigate();
 
   const handleChangeChannel = (channelId: string) => {
@@ -13,31 +13,25 @@ const ChannelListPage = () => {
 
   return (
     <>
-      {/* 채널 목록페이지에서 클릭하면 id를 넘겨주면서 사용*/}
-      {/* 채널 목록페이지가 들어갈 자리*/}
-      {channelLsit && (
-        <>
-          <h1 className="text-2xl">채널 목록</h1>
-          <List>
-            {channelLsit.map((channelItem: Channel) => (
-              <ListItem
-                key={channelItem._id}
-                onClick={() => handleChangeChannel(channelItem._id)}
-                className="rounded-md border-2 p-2"
-              >
-                <label>채널 이름</label>
-                <p>{channelItem.name}</p>
+      <h1 className="text-2xl">채널 목록</h1>
+      <List>
+        {channels.map((channel: Channel) => (
+          <ListItem
+            key={channel._id}
+            onClick={() => handleChangeChannel(channel._id)}
+            className="rounded-md border-2 p-2"
+          >
+            <label>채널 이름</label>
+            <p>{channel.name}</p>
 
-                <label>채널 설명</label>
-                <p>{channelItem.description}</p>
+            <label>채널 설명</label>
+            <p>{channel.description}</p>
 
-                <label>생성 시간</label>
-                <p>{channelItem.createdAt}</p>
-              </ListItem>
-            ))}
-          </List>
-        </>
-      )}
+            <label>생성 시간</label>
+            <p>{channel.createdAt}</p>
+          </ListItem>
+        ))}
+      </List>
     </>
   );
 };
--- src/pages/PostListPage.tsx
+++ src/pages/PostListPage.tsx
@@ -1,12 +1,11 @@
 import { useLocation } from 'react-router-dom';
 import { List, ListItem } from '~/components/domain/List';
 import { useGetPosts } from '~/services';
-import { Post } from '~/types/model';
 
 const PostListPage = () => {
-  const location = useLocation();
-  const { data: postList } = useGetPosts({
-    channelId: location.pathname.split('/')[2]
+  const { pathname } = useLocation();
+  const { data: posts = [] } = useGetPosts({
+    channelId: pathname.split('/')[2] ?? ''
   });
 
   console.log(location.pathname.split('/')[2]);
@@ -23,21 +22,17 @@ const PostListPage = () => {
 
   return (
     <>
-      {postList && (
-        <>
-          <h1 className="text-2xl">채널에 해당하는 포스트 목록</h1>
-          <List>
-            {postList.data.map((postItem: Post) => (
-              <ListItem key={postItem._id} className="rounded-md border-2 p-2">
-                <label>포스트 제목</label>
-                <p>{getPostTitle(postItem.title).title || '제목 없음'}</p>
-                <label>포스트 내용</label>
-                <p>{getPostTitle(postItem.title).content || '내용 없음'}</p>
-              </ListItem>
-            ))}
-          </List>
-        </>
-      )}
+      <h1 className="text-2xl">채널에 해당하는 포스트 목록</h1>
+      <List>
+        {posts.map((post) => (
+          <ListItem key={post._id} className="rounded-md border-2 p-2">
+            <label>포스트 제목</label>
+            <p>{post.title?.trim() || '제목 없음'}</p>
+            <label>포스트 내용</label>
+            <p>{post.content?.trim() || '내용 없음'}</p>
+          </ListItem>
+        ))}
+      </List>
     </>
   );
 };
