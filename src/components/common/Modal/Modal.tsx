6f6676c5e68c4aa6c86fa8f677e5c14337f74848
HongGunWoo <foxrain.gg@gmail.com>
2023-09-19 14:42:55 +0900

    refactor: Modal 수정

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -24,8 +24,8 @@ const Modal = ({
 
   return createPortal(
     <div>
-      <div className="z-2 fixed bottom-0 left-0 right-0 top-0 bg-black bg-opacity-30" />
-      <div className="z-3 fixed left-1/2 top-1/2 h-[27.375rem] w-[21.375rem] -translate-x-1/2 -translate-y-1/2 transform rounded-2xl bg-white p-4">
+      <div className="fixed bottom-0 left-0 right-0 top-0 z-40 bg-black bg-opacity-30" />
+      <div className="fixed left-1/2 top-1/2 z-50 h-[27.375rem] w-[21.375rem] -translate-x-1/2 -translate-y-1/2 transform rounded-2xl bg-white p-4">
         <div className="relative p-2">
           <CloseIcon
             className="absolute right-[0.5rem]"
--- src/components/domain/Footer/Footer.tsx
+++ src/components/domain/Footer/Footer.tsx
@@ -1,5 +1,7 @@
 import { Link, useLocation } from 'react-router-dom';
 import { ChatIcon, HomeIcon, PencilIcon, PersonIcon } from '~/assets';
+import { LoginForm, Modal } from '~/components/common';
+import { useAuth, useModal, useUser } from '~/hooks';
 
 const NavList = [
   {
@@ -26,30 +28,59 @@ const NavList = [
 
 const Footer = () => {
   const { pathname } = useLocation();
+  const { modalOpened, openModal, closeModal } = useModal();
+  const { user } = useUser();
 
   return (
     <nav className="fixed bottom-0 z-50 flex h-24 w-screen max-w-[767px] border-t-2 bg-white pt-4">
       {NavList.map(({ Icon, text, link }) => (
         <div key={text} className="flex h-12 grow items-center justify-center">
-          <Link
-            to={link}
-            className="flex w-20 flex-col items-center border-none bg-white sm:w-24"
-          >
-            <Icon
-              className={
-                pathname === link
-                  ? 'fill-main-darken stroke-main-darken'
-                  : undefined
-              }
-            />
-            <p
-              className={`mt-1 text-xs ${
-                pathname === link ? 'text-main-darken' : 'text-gray-600'
-              }`}
+          {text === '내 프로필' && !user ? (
+            <>
+              <Modal modalOpened={modalOpened} handleClose={closeModal}>
+                <LoginForm handleClose={closeModal} />
+              </Modal>
+              <button
+                className="flex w-20 flex-col items-center border-none bg-white sm:w-24"
+                onClick={openModal}
+              >
+                <Icon
+                  className={
+                    pathname === link
+                      ? 'fill-main-darken stroke-main-darken'
+                      : undefined
+                  }
+                />
+                <p
+                  className={`mt-1 text-xs ${
+                    pathname === link ? 'text-main-darken' : 'text-gray-600'
+                  }`}
+                >
+                  {text}
+                </p>
+              </button>
+            </>
+          ) : (
+            <Link
+              to={link}
+              className="flex w-20 flex-col items-center border-none bg-white sm:w-24"
             >
-              {text}
-            </p>
-          </Link>
+              <Icon
+                className={
+                  pathname === link
+                    ? 'fill-main-darken stroke-main-darken'
+                    : undefined
+                }
+              />
+              <p
+                className={`mt-1 text-xs ${
+                  pathname === link ? 'text-main-darken' : 'text-gray-600'
+                }`}
+              >
+                {text}
+              </p>
+            </Link>
+          )}
         </div>
       ))}
     </nav>
