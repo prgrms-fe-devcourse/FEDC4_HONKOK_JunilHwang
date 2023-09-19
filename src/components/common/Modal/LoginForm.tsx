114abbbc7d1c7cde520781dd73deba3e967e739e
jkea1 <99384699+jkea1@users.noreply.github.com>
2023-09-19 13:52:11 +0900

    style: 로그인 모달창 스타일 적용 (#153)

--- src/components/common/Modal/LoginForm.tsx
+++ src/components/common/Modal/LoginForm.tsx
@@ -1,5 +1,5 @@
 import { useState, ChangeEvent, FormEvent } from 'react';
-
+import { Input, Button } from '~/components/common';
 interface LoginFormProps {
   onSubmit: (email: string, password: string) => void;
 }
@@ -27,26 +27,41 @@ const LoginForm = ({ onSubmit }: LoginFormProps) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <section>
-        <label>Email</label>
-        <input
+    <form className="relative  top-[3rem] w-full" onSubmit={handleSubmit}>
+      <div className="mb-1 p-3 text-2xl font-semibold">로그인</div>
+      <section className="mb-2">
+        <Input
+          className="relative left-[0.3rem] m-1 w-[17rem]"
           type="email"
           value={email}
-          placeholder="이메일을 입력하세요"
+          placeholder="이메일을 입력해주세요."
           onChange={handleChangeEmail}
         />
+        <div className="relative left-[1.25rem] text-[0.75rem] font-medium text-sub-red">
+          영문만 입력 가능합니다.
+        </div>
       </section>
       <section>
-        <label>Password</label>
-        <input
+        <Input
+          className="relative left-[0.3rem] m-1 w-[17rem]"
           type="password"
           value={password}
-          placeholder="비밀번호를 입력하세요"
+          placeholder="비밀번호를 입력해주세요."
           onChange={handleChangePassword}
         />
+        <div className="relative left-[1.25rem] text-[0.75rem] font-medium text-sub-red">
+          8자 이상의 영문+숫자 조합으로 입력해주세요.
+        </div>
       </section>
-      <button type="submit">Sign In</button>
+      <Button
+        theme="main"
+        className="relative top-[3rem] h-[3.5rem] w-[18rem] rounded-[0.625rem] text-lg"
+      >
+        로그인
+      </Button>
+      <div className="relative right-[1rem] top-[3.5rem] text-right text-sm">
+        회원가입
+      </div>
     </form>
   );
 };
--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren } from 'react';
 import { createPortal } from 'react-dom';
+import { CloseIcon } from '~/assets';
 
 interface ModalProps {
   modalOpened: boolean;
@@ -26,10 +27,11 @@ const Modal = ({
       <div className="z-2 fixed bottom-0 left-0 right-0 top-0 bg-black bg-opacity-30" />
       <div className="z-3 fixed left-1/2 top-1/2 h-[27.375rem] w-[21.375rem] -translate-x-1/2 -translate-y-1/2 transform rounded-2xl bg-white p-4">
         <div className="relative p-2">
-          <span className="absolute right-[0.5rem] " onClick={handleClose}>
-            X
-          </span>
-          <div className="absolute top-[5rem]">{children}</div>
+          <CloseIcon
+            className="absolute right-[0.5rem]"
+            onClick={handleClose}
+          />
+          <div>{children}</div>
         </div>
       </div>
     </div>,
