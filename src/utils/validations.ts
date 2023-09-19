5dfa64ace605eff592c2bb7eec39ebc52e1a095c
backward99 <wjeeho@naver.com>
2023-09-20 00:42:58 +0900

    feat: 글 유효성 검사

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -5,6 +5,12 @@ interface SignUp {
   isPasswordValid: boolean;
 }
 
+interface CreatePost {
+  title: string;
+  content: string;
+  channelId: string;
+}
+
 const isValidEmail = (email: string) => {
   const emailPattern = /^[a-zA-Z0-9]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
@@ -18,28 +24,38 @@ const isValidPassword = (password: string) => {
   return passwordPattern.test(password);
 };
 
-const isValidFullname = (fullname: string) => {
+const isValidFullName = (fullName: string) => {
   const fullNamePattern = /^[A-Za-z0-9_\uAC00-\uD7A3]+$/;
 
   return (
-    fullNamePattern.test(fullname) &&
-    fullname.length >= 1 &&
-    fullname.length <= 8
+    fullNamePattern.test(fullName) &&
+    fullName.length >= 1 &&
+    fullName.length <= 8
   );
 };
 
-const isVaildSignUp = ({
+const isValidSignUp = ({
   email,
   password,
   fullName,
   isPasswordValid
 }: SignUp) => {
   return (
-    isValidFullname(fullName) &&
+    isValidFullName(fullName) &&
     isValidPassword(password) &&
     isValidEmail(email) &&
     isPasswordValid
   );
 };
 
-export { isValidEmail, isValidPassword, isValidFullname, isVaildSignUp };
+const isValidCreatePost = ({ channelId, title, content }: CreatePost) => {
+  return channelId && title.length >= 3 && content.length >= 10;
+};
+
+export {
+  isValidEmail,
+  isValidPassword,
+  isValidFullName,
+  isValidSignUp,
+  isValidCreatePost
+};
