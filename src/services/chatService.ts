02ddd19cc1906db13e563063cb8d896e6c7f390e
jgjgill <dbdltm22@naver.com>
2023-09-20 16:50:16 +0900

    chore: 사용되는 api 관련 함수 기본 정의

---
+++ src/services/chatService.ts
@@ -0,0 +1,17 @@
+const chatKeys = {};
+
+const getMyConversations = async () => {};
+
+const getChat = async () => {};
+
+const createMessage = async () => {};
+
+const putMessageUpdateSeen = async () => {};
+
+export const useGetMyChats = () => {};
+
+export const useGetChat = () => {};
+
+export const useCreateMessage = () => {};
+
+export const usePutMessageUpdateSeen = () => {};
