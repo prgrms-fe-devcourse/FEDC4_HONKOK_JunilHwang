5a52ab181611e53b97db8b4b8d3dfd1b04c877f9
backward99 <wjeeho@naver.com>
2023-09-20 12:15:16 +0900

    refactor: 사용하지 않아도 되는 요소 제거, 상황에 어울리는 스타일 속성으로 변경

--- src/components/domain/Header/Header.tsx
+++ src/components/domain/Header/Header.tsx
@@ -26,37 +26,34 @@ const Header = ({
 
   return (
     <>
-      <div className="h-28" />
-      <article className="fixed left-1/2 top-0 z-50 mb-48 w-screen max-w-[767px] -translate-x-1/2">
-        <section className="relative flex h-28 w-full justify-center bg-main-lighten text-white">
-          <h1 className="mt-14 inline-block text-xl font-bold">{children}</h1>
-          {leftArea === 'home' ? (
-            <HomeIcon className="absolute left-6 top-14 h-6 w-6 stroke-white" />
-          ) : (
-            <LeftArrowIcon
-              className="absolute left-6 top-14 h-6 w-6 fill-white"
-              onClick={handleGoBack}
+      <header className="sticky top-0 z-50 flex h-28 w-screen max-w-[767px] justify-center bg-main-lighten text-white">
+        <h1 className="mt-14 inline-block text-xl font-bold">{children}</h1>
+        {leftArea === 'home' ? (
+          <HomeIcon className="absolute left-6 top-14 h-6 w-6 stroke-white" />
+        ) : (
+          <LeftArrowIcon
+            className="absolute left-6 top-14 h-6 w-6 fill-white"
+            onClick={handleGoBack}
+          />
+        )}
+        {rightArea && (
+          <>
+            <SearchIcon
+              className="absolute right-16 top-14 h-6 w-6 stroke-white"
+              onClick={() => handleGoToPage('search')}
             />
-          )}
-          {rightArea && (
-            <>
-              <SearchIcon
-                className="absolute right-16 top-14 h-6 w-6 stroke-white"
-                onClick={() => handleGoToPage('search')}
-              />
-              <BellIcon
-                className="absolute right-6 top-14 h-6 w-6 stroke-white"
-                onClick={() => handleGoToPage('notifications')}
-              />
-              {notification.length !== 0 && (
-                <Badge className="absolute right-6 top-12 flex translate-x-1/4 translate-y-1/4 items-center justify-center border-none text-[10px] text-white cs:bg-active-base cs:px-1 cs:py-0">
-                  {notification.length}
-                </Badge>
-              )}
-            </>
-          )}
-        </section>
-      </article>
+            <BellIcon
+              className="absolute right-6 top-14 h-6 w-6 stroke-white"
+              onClick={() => handleGoToPage('notifications')}
+            />
+            {notification.length !== 0 && (
+              <Badge className="absolute right-6 top-12 flex translate-x-1/4 translate-y-1/4 items-center justify-center border-none text-[10px] text-white cs:bg-active-base cs:px-1 cs:py-0">
+                {notification.length}
+              </Badge>
+            )}
+          </>
+        )}
+      </header>
     </>
   );
 };
