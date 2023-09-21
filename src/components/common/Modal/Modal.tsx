440436315d4892b132e9896c3d5381eb99ad1968
backward99 <wjeeho@naver.com>
2023-09-21 18:15:11 +0900

    style: 모달 높이, 넓이 고정 해제

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -25,14 +25,9 @@ const Modal = ({
   return createPortal(
     <div>
       <div className="fixed bottom-0 left-0 right-0 top-0 z-50 bg-black bg-opacity-30" />
-      <div className="fixed left-1/2 top-1/2 z-50 h-[27.375rem] w-[21.375rem] -translate-x-1/2 -translate-y-1/2 transform rounded-2xl bg-white p-4">
-        <div className="relative p-2">
-          <CloseIcon
-            className="absolute right-[0.5rem]"
-            onClick={handleClose}
-          />
-          <div>{children}</div>
-        </div>
+      <div className="fixed left-1/2 top-1/2 z-50 flex w-fit min-w-[19rem] -translate-x-1/2 -translate-y-1/2 transform flex-col rounded-2xl bg-white p-4">
+        <CloseIcon className="z-50 self-end" onClick={handleClose} />
+        <div className="relative h-full flex-grow p-2">{children}</div>
       </div>
     </div>,
     modalRoot
