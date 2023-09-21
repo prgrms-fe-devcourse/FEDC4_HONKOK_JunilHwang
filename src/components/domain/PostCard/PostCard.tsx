720a7da4a32f90d561dfac69f6b58201f65b6a17
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-09-22 07:44:15 +0900

    style: 채널별 기본 이미지 적용 (#186)
    * style: 채널별 기본 이미지 에셋 추가
    
    * style: Post 관련 컴포넌트 스타일 수정
    
    * feat: 채널별 기본 이미지 적용

--- src/components/domain/PostCard/PostCard.tsx
+++ src/components/domain/PostCard/PostCard.tsx
@@ -1,3 +1,4 @@
+import { DEFAULT_IMAGES } from './constants';
 import { CommentIcon, HeartIcon } from '~/assets';
 import { Badge, Image } from '~/components/common';
 import { Post } from '~/types';
@@ -16,41 +17,50 @@ const PostCard = ({
   channel,
   comments,
   createdAt,
+  image,
   likes,
   title,
   handleClick
 }: PostCardProps) => {
   return (
     <li>
-      <button onClick={handleClick} className="h-40 w-40">
-        <div className="relative">
-          <Image src="https://storage.enuri.info/pic_upload/knowbox2/202208/023349314202208210721f7ed-31bd-45f8-8a1b-7faf2adc2e45.jpg" />
-          <Badge variant="subtle" className="absolute left-2 top-2">
-            {channel.name}
-          </Badge>
-        </div>
+      <button
+        onClick={handleClick}
+        className="relative h-28 w-full rounded-lg border-2 border-gray-200"
+      >
+        <Image
+          src={
+            image
+              ? image
+              : DEFAULT_IMAGES[channel.name as keyof typeof DEFAULT_IMAGES]
+          }
+          className="h-full w-full object-cover"
+        />
+        <Badge variant="subtle" className="absolute left-2 top-2">
+          {channel.name}
+        </Badge>
+      </button>
 
-        <div className="px-1 pt-2">
-          <p className="overflow-hidden text-ellipsis whitespace-nowrap text-left text-xs text-gray-500">
-            {title}
-          </p>
+      <div className="px-1 pt-2">
+        <p className="overflow-hidden text-ellipsis whitespace-nowrap text-left text-xs text-gray-500">
+          {title}
+        </p>
 
-          <div className="flex items-center justify-between text-[0.5625rem] text-gray-400">
-            <span>{getRelativeTime(createdAt)}</span>
-            <div className="flex gap-2">
-              <div className="flex items-center gap-1">
-                <HeartIcon className="h-3 w-3 fill-gray-400" />
-                <div>{likes.length}</div>
-              </div>
+        <div className="flex items-center justify-between text-[0.5625rem] text-gray-400">
+          <span>{getRelativeTime(createdAt)}</span>
+          <div className="flex gap-2">
+            <div className="flex items-center gap-1">
+              <HeartIcon className="h-3 w-3 fill-gray-400" />
+              <div>{likes.length}</div>
+            </div>
 
-              <div className="flex items-center gap-1">
-                <CommentIcon className="h-3 w-3 fill-gray-400" />
-                <div>{comments.length}</div>
-              </div>
+            <div className="flex items-center gap-1">
+              <CommentIcon className="h-3 w-3 fill-gray-400" />
+              <div>{comments.length}</div>
             </div>
           </div>
         </div>
-      </button>
+      </div>
     </li>
   );
 };
