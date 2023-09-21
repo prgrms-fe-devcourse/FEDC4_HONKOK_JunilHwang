74b77bd569fb8f3ea130cf8a1ebe29dd6866a43b
backward99 <86753969+backward99@users.noreply.github.com>
2023-09-21 23:00:39 +0900

    refactor: Signuppage 피드백 반영 (#187)
    * refactor: 뒤로가기 버튼 크기 수정
    
    * feat: 회원가입 클릭시 페이지 이동
    
    * refactor: 이메일, 비밀번호, 닉네임 유효성 검사 변경
    
    * refactor: 아이콘 커서, 위치 변경, label 이름 변경, palceholder 변경

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -12,26 +12,22 @@ interface CreatePost {
 }
 
 const isValidEmail = (email: string) => {
-  const emailPattern = /^[a-zA-Z0-9]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const emailPattern =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   return emailPattern.test(email.toLowerCase());
 };
 
 const isValidPassword = (password: string) => {
-  const passwordPattern =
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[a-zA-Z\d@$!%*?&]{8,}$/;
+  const passwordPattern = /^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9@$!%*?&]{8,}$/;
 
   return passwordPattern.test(password);
 };
 
 const isValidFullName = (fullName: string) => {
-  const fullNamePattern = /^[A-Za-z0-9_\uAC00-\uD7A3]+$/;
+  const fullNamePattern = /^[A-Za-z0-9_\uAC00-\uD7A3\u3131-\u3163]{1,8}$/;
 
-  return (
-    fullNamePattern.test(fullName) &&
-    fullName.length >= 1 &&
-    fullName.length <= 8
-  );
+  return fullNamePattern.test(fullName);
 };
 
 const isValidSignUp = ({
