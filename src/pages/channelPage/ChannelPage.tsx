90b9ec615facac25ae7fe8fdb0bf1d7300c70b0b
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-09-22 18:28:59 +0900

    feat: 채널 페이지 navigate 기능 추가

--- src/pages/channelPage/ChannelPage.tsx
+++ src/pages/channelPage/ChannelPage.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { CHANNELS } from './constants';
 import { Header, PostCard } from '~/components/domain';
 import { PostList } from '~/components/domain/PostList';
@@ -6,12 +6,18 @@ import { useGetPosts } from '~/services';
 
 const ChannelPage = () => {
   const location = useLocation();
+  const navigate = useNavigate();
+
   const channel = location.pathname.split('/')[2] as keyof typeof CHANNELS;
 
   const { data: posts } = useGetPosts({
     channelId: CHANNELS[channel].id,
     limit: 6
   });
+
+  const navigateToPost = (postId: string) => {
+    navigate(`/posts/${postId}`);
+  };
 
   return (
     <div className="h-full overflow-y-scroll">
@@ -20,7 +26,7 @@ const ChannelPage = () => {
         title="채널글 보기"
         posts={posts}
         RenderComponent={(post) => (
-          <PostCard {...post} handleClick={() => console.log(post._id)} />
+          <PostCard {...post} handleClick={() => navigateToPost(post._id)} />
         )}
       />
     </div>
