3bcfc43b9a092882a5eaf57ea3b15d2aa2501d58
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-09-22 17:10:06 +0900

    feat: 알림 생성 api 기능 구현

--- src/services/notificationService.ts
+++ src/services/notificationService.ts
@@ -2,6 +2,13 @@ import { useMutation, useQuery } from '@tanstack/react-query';
 import { snsApiClient } from '~/api';
 import { Notification, User } from '~/types';
 
+interface CreateNotification {
+  notificationType: 'COMMENT' | 'FOLLOW' | 'LIKE' | 'MESSAGE';
+  notificationTypeId: string;
+  userId: string;
+  postId: string | null;
+}
+
 const notificationKeys = {
   all: ['notifications'] as const
 };
@@ -14,6 +21,20 @@ const getNotifications = async (): Promise<Notification[]> => {
 
 const putNotificationsSeen = async () => {
   return snsApiClient.put('/notifications/seen');
+};
+
+const createNotification = async ({
+  notificationType,
+  notificationTypeId,
+  userId,
+  postId
+}: CreateNotification) => {
+  return snsApiClient.post('/notifications/create', {
+    notificationType,
+    notificationTypeId,
+    userId,
+    postId
+  });
 };
 
 export const useGetNotifications = (user: User) => {
@@ -30,4 +51,8 @@ export const useGetNotifications = (user: User) => {
 
 export const usePutNotificationsSeen = () => {
   return useMutation({ mutationFn: putNotificationsSeen });
+};
+
+export const useCreateNotification = () => {
+  return useMutation({ mutationFn: createNotification });
 };
