0189efae3b3d25a0cadd00cd149d066c3986d84b
backward99 <wjeeho@naver.com>
2023-09-23 04:29:28 +0900

    refactor: 글 수정 페이지 정리

--- src/pages/PostEditPage/PostEditPage.tsx
+++ src/pages/PostEditPage/PostEditPage.tsx
@@ -1,18 +1,215 @@
+import { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
-import { PostInfo } from '../PostCreatePage';
-import { useHandlePostEdit } from '~/hooks';
+import { useNavigate } from 'react-router-dom';
+import { ChannelList } from '../PostCreatePage';
+import { ImageIcon } from '~/assets';
+import {
+  Button,
+  HorizontalScroll,
+  Input,
+  Menu,
+  useToast
+} from '~/components/common';
+import { Header } from '~/components/domain';
+import { useModal } from '~/hooks';
 import { useGetPost } from '~/services';
+import { useEditPost, useGetImageFile } from '~/services';
+import { isValidCreatePost } from '~/utils';
 import assert from '~/utils/assert';
 
 const PostEditPage = () => {
-  const { postId = '' } = useParams();
-  const { data: post } = useGetPost(postId);
+  const { postId: pagePostId = '' } = useParams();
+  const { data: post } = useGetPost(pagePostId);
 
   assert(post);
 
-  const postInfo = useHandlePostEdit({ ...post });
+  const {
+    title: prevPostTitle,
+    content: prevPostContent = '',
+    image: prevPostImageUrl = '',
+    imagePublicId: prevPostImagePublicId,
+    channel: prevChannel,
+    _id: postId
+  } = post;
 
-  return <PostInfo {...postInfo} />;
+  const { data: prevPostImageFile } = useGetImageFile(prevPostImageUrl);
+
+  const { mutate: editPost } = useEditPost();
+
+  const elementRef = useRef<HTMLDivElement>(null);
+  const imageInputRef = useRef<HTMLInputElement>(null);
+
+  const [title, setTitle] = useState(prevPostTitle);
+  const [content, setContent] = useState(prevPostContent);
+  const [channelId, setChannelId] = useState(prevChannel._id);
+  const [file, setFile] = useState<File | undefined>();
+  const [image, setImage] = useState<string | undefined>(prevPostImageUrl);
+
+  const { addToast } = useToast();
+
+  const navigate = useNavigate();
+
+  const { modalOpened, openModal, closeModal } = useModal();
+
+  const editURL = (selectedFile: File | null) => {
+    if (!selectedFile) {
+      return undefined;
+    }
+
+    return URL.createObjectURL(selectedFile);
+  };
+
+  const handleTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.currentTarget.value);
+  };
+
+  const handleContent = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(event.currentTarget.value);
+  };
+
+  const handleImageRemove = () => {
+    setImage(undefined);
+    setFile(undefined);
+  };
+
+  const handleImageFilesChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (!event.target.files) {
+      return;
+    }
+
+    setImage(editURL(event.target.files[0]));
+    setFile(event.target.files[0]);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!isValidCreatePost({ title, content, channelId })) {
+      return;
+    }
+
+    const newPost = {
+      postId,
+      title,
+      content,
+      image: file,
+      imageToDeletePublicId: file ? undefined : prevPostImagePublicId,
+      channelId
+    };
+
+    editPost(newPost, {
+      onError: () => {
+        addToast({
+          content: '글 수정에 실패했습니다. 잠시 후에 다시 시도 하세요.'
+        });
+      },
+      onSuccess: ({ data }) => {
+        navigate(`/posts/${data._id}`);
+      }
+    });
+  };
+
+  const handleImageInputClick = () => {
+    if (!imageInputRef.current) {
+      return;
+    }
+
+    imageInputRef.current.click();
+  };
+
+  useEffect(() => {
+    setFile(prevPostImageFile);
+  }, [prevPostImageFile]);
+
+  return (
+    <>
+      <Header leftArea="left-arrow" rightArea={false}>
+        글 작성하기
+      </Header>
+      <article className="relative p-5">
+        <form onSubmit={handleSubmit}>
+          <section className="pb-5">
+            <p className="mb-2">채널 선택</p>
+            <HorizontalScroll>
+              <ChannelList channelId={channelId} handleClick={setChannelId} />
+            </HorizontalScroll>
+          </section>
+          <section className="relative">
+            <Input
+              value={title}
+              onChange={handleTitle}
+              placeholder="제목을 입력해주세요."
+              className="mb-5 w-full"
+            />
+            <section className="mb-[1.63rem] flex gap-[0.81rem] overflow-auto whitespace-nowrap">
+              <Input
+                ref={imageInputRef}
+                onChange={handleImageFilesChange}
+                className="hidden"
+                type="file"
+                accept="image/*"
+              />
+              {image ? (
+                <div ref={elementRef}>
+                  <div className="relative aspect-[5/3] w-full flex-shrink-0 overflow-hidden rounded-xl bg-gray-100">
+                    <img
+                      className="aspect-[5/3] w-full object-cover"
+                      src={image}
+                      alt="이미지 미리보기"
+                    />
+                    <Button
+                      type="button"
+                      className="absolute bottom-1 right-1 flex h-12 w-12 items-center justify-center rounded-full bg-white"
+                      onClick={openModal}
+                    >
+                      <ImageIcon className="h-6 w-6 stroke-gray-400" />
+                    </Button>
+                  </div>
+                </div>
+              ) : (
+                <div
+                  onClick={handleImageInputClick}
+                  className="flex aspect-[5/3] w-full flex-shrink-0 flex-col items-center justify-center rounded-[0.3125rem] bg-gray-100"
+                >
+                  <ImageIcon className="stroke-gray-400" />
+                  <span className="text-4 text-gray-400">사진 추가</span>
+                </div>
+              )}
+            </section>
+            <textarea
+              value={content}
+              onChange={handleContent}
+              placeholder="내용을 작성해보세요. - 10글자 이상"
+              className="w-full resize-none rounded-[0.625rem] border-[1.5px] border-gray-200 pb-[0.56rem] pl-[0.87rem] pt-[0.5rem] text-xs placeholder:text-gray-200 focus:outline-main-base cs:h-40"
+            />
+            <Button
+              theme="main"
+              className="fixed bottom-8 right-6 h-10 w-16 transition-none disabled:opacity-50 md:right-1/2 md:translate-x-[22.5rem]"
+              disabled={!isValidCreatePost({ title, content, channelId })}
+            >
+              등록
+            </Button>
+            {modalOpened && (
+              <Menu
+                className="right-2 h-20 w-32 text-center"
+                portalTarget={elementRef.current!}
+                handleClose={closeModal}
+              >
+                <Menu.Item handleClick={handleImageInputClick}>
+                  이미지 변경
+                </Menu.Item>
+                <Menu.Item handleClick={handleImageRemove}>
+                  이미지 삭제
+                </Menu.Item>
+              </Menu>
+            )}
+          </section>
+        </form>
+      </article>
+    </>
+  );
 };
 
 export default PostEditPage;
