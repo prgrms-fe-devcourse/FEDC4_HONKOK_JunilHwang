fb57e7c7bbd75ff6d5a2d5df1eeea398b21658d9
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-09-22 20:32:11 +0900

    feat: 알림 페이지 구현

--- src/pages/NotificationsPage/NotificationsPage.tsx
+++ src/pages/NotificationsPage/NotificationsPage.tsx
@@ -1,9 +1,13 @@
 import { useEffect } from 'react';
+import { NotificationList } from './components';
 import { Header } from '~/components/domain';
+import { useUserNotifications } from '~/hooks';
 import { usePutNotificationsSeen } from '~/services';
 
 const NotificationsPage = () => {
   const { mutate: putNotificationsSeen } = usePutNotificationsSeen();
+
+  const notifications = useUserNotifications();
 
   useEffect(() => {
     putNotificationsSeen();
@@ -14,6 +18,8 @@ const NotificationsPage = () => {
       <Header leftArea="left-arrow" rightArea={false}>
         알림
       </Header>
+
+      <NotificationList notifications={notifications} />
     </div>
   );
 };
