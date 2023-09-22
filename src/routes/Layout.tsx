d6ae665866ff4e498621ec947189ef1e63ad31bb
jgjgill <dbdltm22@naver.com>
2023-09-22 14:34:49 +0900

    loading 관련 코드 제거

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,11 +1,6 @@
 import { Outlet } from 'react-router-dom';
-import { useUser } from '~/hooks';
 
 const Layout = () => {
-  const { userIsLoading } = useUser();
-
-  if (userIsLoading) return;
-
   return (
     <div className="mx-auto h-screen max-w-[767px] pb-24 pt-28 font-OAGothic">
       <Outlet />
