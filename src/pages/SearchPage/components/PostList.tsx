6d9803732eceb4f7735d413d07cebb38b9f293f2
backward99 <wjeeho@naver.com>
2023-09-24 00:53:14 +0900

    feat: Post 검색 결과

---
+++ src/pages/SearchPage/components/PostList.tsx
@@ -0,0 +1,59 @@
+import { useNavigate } from 'react-router-dom';
+import { CHANNEL_NAMES } from '../constants';
+import { PersonIcon } from '~/assets';
+import { Image } from '~/components/common';
+import { Post } from '~/types';
+import { getRelativeTime } from '~/utils';
+
+interface PostListProps {
+  slice?: boolean;
+  parsedPostResults: Post[];
+}
+
+const PostList = ({ slice = false, parsedPostResults }: PostListProps) => {
+  const navigate = useNavigate();
+
+  const handlePostClick = (postId: string) => {
+    navigate(`/posts/${postId}`);
+  };
+
+  const postResults = slice
+    ? parsedPostResults.slice(0, 2)
+    : parsedPostResults.slice();
+
+  return postResults.map((post) => {
+    return (
+      <a
+        className="flex w-full items-center gap-4 text-xs"
+        key={post._id}
+        onClick={() => handlePostClick(post._id)}
+      >
+        {post.image ? (
+          <Image
+            className="max-h-[2.25rem] max-w-[2.25rem] cs:rounded-full"
+            src={post.image}
+            alt={post.image}
+          />
+        ) : (
+          <div className="flex h-9 w-9 flex-shrink-0 items-center justify-center rounded-full bg-gray-200">
+            <PersonIcon className=" h-6 w-4 flex-shrink-0 fill-gray-300 stroke-none cs:rounded-full" />
+          </div>
+        )}
+
+        <div className="flex flex-grow flex-col justify-center gap-1 ">
+          <span className="line-clamp-1 overflow-hidden">{post.title}</span>
+          <span className=" text-gray-400">
+            {typeof post.channel === 'string'
+              ? CHANNEL_NAMES[post.channel]
+              : null}
+          </span>
+        </div>
+        <span className="flex-shrink-0  text-slate-300">
+          {getRelativeTime(post.createdAt)}
+        </span>
+      </a>
+    );
+  });
+};
+
+export default PostList;
