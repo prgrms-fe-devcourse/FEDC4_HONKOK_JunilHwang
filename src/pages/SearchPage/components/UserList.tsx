1b667362d3ee2ed996d5676f971bc8fc735db0b4
backward99 <wjeeho@naver.com>
2023-09-24 00:53:27 +0900

    feat: 유저 검색 결과

---
+++ src/pages/SearchPage/components/UserList.tsx
@@ -0,0 +1,52 @@
+import { useNavigate } from 'react-router-dom';
+import { PersonIcon } from '~/assets';
+import { Image } from '~/components/common';
+import { User } from '~/types';
+import { getRelativeTime } from '~/utils';
+
+interface UserListProps {
+  slice?: boolean;
+  userResults: User[];
+}
+
+const UserList = ({ slice = false, userResults }: UserListProps) => {
+  const navigate = useNavigate();
+
+  const handleProfileClick = (userId: string) => {
+    navigate(`/profile/${userId}`);
+  };
+
+  const usersInfo = slice ? userResults.slice(0, 2) : userResults.slice();
+
+  return usersInfo.map((user) => {
+    return (
+      <a
+        className="flex w-full items-center gap-4"
+        key={user._id}
+        onClick={() => handleProfileClick(user._id)}
+      >
+        {user.image ? (
+          <Image
+            className="max-h-[2.25rem] max-w-[2.25rem] cs:rounded-full"
+            src={user.image}
+            alt={user.image}
+          />
+        ) : (
+          <div className="flex h-9 w-9 flex-shrink-0 items-center justify-center rounded-full bg-gray-200">
+            <PersonIcon className=" h-6 w-4 flex-shrink-0 fill-gray-300 stroke-none cs:rounded-full" />
+          </div>
+        )}
+
+        <div className="flex flex-grow flex-col justify-center gap-1 ">
+          <span className="line-clamp-1 overflow-hidden">{user.fullName}</span>
+          <span className="text-gray-400">{user.followers.length}</span>
+        </div>
+        <span className="flex-shrink-0 text-slate-300">
+          {getRelativeTime(user.createdAt)}
+        </span>
+      </a>
+    );
+  });
+};
+
+export default UserList;
