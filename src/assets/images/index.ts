2cd743a7f03ac0133d47534fff1ca35aa808afef
backward99 <wjeeho@naver.com>
2023-09-24 16:19:52 +0900

    chore: 이미지 파일 내보내기

---
+++ src/assets/images/index.ts
@@ -0,0 +1,20 @@
+import CleanImage from './clean.png';
+
+import CookImage from './cook.png';
+import DecorImage from './decor.png';
+import GeneralImage from './general.png';
+import HelpImage from './help.png';
+import { ReactComponent as LogoImage } from './Logo.svg';
+import ProfileImage from './profile.png';
+import SeatedManImage from './seated-man.png';
+
+export {
+  CleanImage,
+  CookImage,
+  DecorImage,
+  GeneralImage,
+  HelpImage,
+  ProfileImage,
+  SeatedManImage,
+  LogoImage
+};
