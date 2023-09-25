6239233b165a481b4d62df1ca816da34de03ce6c
backward99 <wjeeho@naver.com>
2023-09-26 03:08:01 +0900

    style: 검색 결과 없을 때 스타일 수정

--- src/pages/SearchPage/components/PostSearchResult.tsx
+++ src/pages/SearchPage/components/PostSearchResult.tsx
@@ -1,30 +1,35 @@
+import { memo } from 'react';
 import { PostList } from './index';
+import { Exclamation } from '~/components/common/Exclamation';
 import { Post } from '~/types';
 
 interface PostSearchResultProps {
   parsedPostResults: Post[];
 }
 
-const PostSearchResult = ({ parsedPostResults }: PostSearchResultProps) => {
-  return (
-    <section className="relative h-full w-full overflow-hidden">
-      <section className="relative mb-6 flex h-full flex-col justify-evenly overflow-hidden rounded-xl border-[1px]">
-        {parsedPostResults.length > 0 ? (
-          <>
-            <span className="px-4 py-2 text-sm">포스트</span>
-            <section className="flex h-full flex-col overflow-y-auto overflow-x-hidden">
-              <PostList parsedPostResults={parsedPostResults} />
-            </section>
-          </>
-        ) : (
-          <span className="flex h-full w-full items-center justify-center">
-            {' '}
-            결과 없음
-          </span>
-        )}
+const PostSearchResult = memo(
+  ({ parsedPostResults }: PostSearchResultProps) => {
+    return (
+      <section className="relative h-full w-full overflow-x-hidden">
+        <section className="relative mb-6 flex flex-col justify-evenly overflow-hidden rounded-xl border-[1px] bg-white text-xs">
+          <span className="px-4 py-2 text-sm">포스트</span>
+          {parsedPostResults.length > 0 ? (
+            <>
+              <section className="flex h-full flex-col overflow-y-auto overflow-x-hidden">
+                <PostList parsedPostResults={parsedPostResults} />
+              </section>
+            </>
+          ) : (
+            <Exclamation className="mb-9 mt-4">
+              <p className="text-[0.875rem] text-gray-400">
+                검색 결과가 없습니다.
+              </p>
+            </Exclamation>
+          )}
+        </section>
       </section>
-    </section>
-  );
-};
+    );
+  }
+);
 
 export default PostSearchResult;
