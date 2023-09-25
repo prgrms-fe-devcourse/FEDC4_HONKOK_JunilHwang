9ad56b7c8efe2b718d23ce32be1d2718fdf71026
backward99 <wjeeho@naver.com>
2023-09-25 23:31:20 +0900

    style: 모달 글씨 적용, 요소 간격 수정

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -28,12 +28,12 @@ const Modal = ({
         className="fixed bottom-0 left-0 right-0 top-0 z-50 bg-black bg-opacity-30"
         onClick={handleClose}
       />
-      <div className="fixed left-1/2 top-1/2 z-50 flex w-fit min-w-[19rem] -translate-x-1/2 -translate-y-1/2 transform flex-col rounded-2xl bg-white p-4">
+      <div className="fixed left-1/2 top-1/2 z-50 flex w-fit min-w-[19rem] -translate-x-1/2 -translate-y-1/2 transform flex-col rounded-2xl bg-white p-6 font-OAGothic">
         <CloseIcon
-          className="z-50 cursor-pointer self-end"
+          className="y-6 z-50 mb-2 w-6 cursor-pointer self-end"
           onClick={handleClose}
         />
-        <div className="relative h-full flex-grow p-2">{children}</div>
+        <div className="relative h-full flex-grow">{children}</div>
       </div>
     </div>,
     modalRoot
