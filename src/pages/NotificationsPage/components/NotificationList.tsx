d55b2d582f7144706d142f505d16380624038a86
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-09-26 02:02:54 +0900

    feat: 알림 없을 때 컴포넌트 추가

--- src/pages/NotificationsPage/components/NotificationList.tsx
+++ src/pages/NotificationsPage/components/NotificationList.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren } from 'react';
 import NotificationItem from './NotificationItem';
+import { Exclamation } from '~/components/common/Exclamation';
 import { Notification } from '~/types';
 
 interface NotificationListProps {
@@ -9,12 +10,23 @@ interface NotificationListProps {
 const NotificationList = ({
   notifications
 }: PropsWithChildren<NotificationListProps>) => {
+  if (notifications.length) {
+    return (
+      <ul className="flex flex-col gap-2 px-6 py-4">
+        {notifications.map((notification) => (
+          <NotificationItem
+            key={notification._id}
+            notification={notification}
+          />
+        ))}
+      </ul>
+    );
+  }
+
   return (
-    <ul className="flex flex-col gap-2 px-6 py-4">
-      {notifications.map((notification) => (
-        <NotificationItem key={notification._id} notification={notification} />
-      ))}
-    </ul>
+    <Exclamation className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <p className="text-[0.875rem] text-gray-400">알림이 없습니다.</p>
+    </Exclamation>
   );
 };
 
