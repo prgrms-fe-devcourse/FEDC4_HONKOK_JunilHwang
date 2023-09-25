2def9aa8eb3a654ca4855d19a937d85b464d69c6
jkea1 <jinkyng7890@gmail.com>
2023-09-26 01:07:16 +0900

    feat: 리다이렉트 경로 추가 합니다.
    - 로그인이 필요한 서비스에 unlogin 인 상태로 접근시 UnLoginLoader를 이용해서 리다이렉트 시킴
    
    - 로그인한 상태로 회원가입 사이트에 접근시 LoginLoader를 이용해서 리다이렉트 시킴

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from 'react-router-dom';
 import { PATH } from './constants';
 import Layout from './Layout';
 import LayoutWithFooter from './LayoutWithFooter';
+import LoginLoader from './LoginLoader';
+import UnLoginLoader from './UnLoginLoader';
 import {
   ChannelPage,
   ChatPage,
@@ -32,17 +34,37 @@ const router = createBrowserRouter([
           { path: PATH.FOLLOW, element: <FollowPage /> },
           { path: PATH.PROFILE, element: <ProfilePage /> },
           { path: PATH.PROFILE_EDIT, element: <ProfileEditPage /> },
-          { path: PATH.LIKE_LIST, element: <LikeListPage /> },
+          {
+            path: PATH.LIKE_LIST,
+            element: <LikeListPage />,
+            loader: UnLoginLoader
+          }, // 보호 메인으로 들어가게 해라
           { path: PATH.CHANNEL, element: <ChannelPage /> },
-          { path: PATH.NOTIFICATIONS, element: <NotificationsPage /> },
-          { path: PATH.CONVERSATIONS, element: <ConversationPage /> }
+          {
+            path: PATH.NOTIFICATIONS,
+            element: <NotificationsPage />,
+            loader: UnLoginLoader
+          },
+          {
+            path: PATH.CONVERSATIONS,
+            element: <ConversationPage />,
+            loader: UnLoginLoader
+          }
         ]
       },
-      { path: PATH.SIGNUP, element: <SignUpPage /> },
+      { path: PATH.SIGNUP, element: <SignUpPage />, loader: LoginLoader },
       { path: PATH.POST, element: <PostPage /> },
-      { path: PATH.POST_EDIT, element: <PostEditPage /> },
-      { path: PATH.POST_CREATE, element: <PostCreatePage /> },
-      { path: PATH.CHAT, element: <ChatPage /> },
+      {
+        path: PATH.POST_EDIT,
+        element: <PostEditPage />,
+        loader: UnLoginLoader
+      },
+      {
+        path: PATH.POST_CREATE,
+        element: <PostCreatePage />,
+        loader: UnLoginLoader
+      },
+      { path: PATH.CHAT, element: <ChatPage />, loader: UnLoginLoader },
       { path: '*', element: <NotFoundPage /> }
     ]
   }
