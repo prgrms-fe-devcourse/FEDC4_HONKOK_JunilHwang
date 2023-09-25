f4a89f86b545c130c3b94e0e6cf0ba9b00e4400f
backward99 <86753969+backward99@users.noreply.github.com>
2023-09-26 03:26:39 +0900

    feat: 검색 페이지 관련 작업 (#258)
    * style: 유저 이미지 찌그러짐 해결
    
    * refactor: 검색어 입력시 헤더 리랜더링 방지
    
    * chore: 전페 페이지 디자인 브랜치에서 떙겨오기
    
    * style: 검색 결과 없을 때 스타일 수정
    
    * refactor: 최적화 작업 진행

--- src/components/domain/Header/Header.tsx
+++ src/components/domain/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { memo } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { BellIcon, LeftArrowIcon, SearchIcon } from '~/assets';
 import { Badge, Button } from '~/components/common';
@@ -8,55 +9,57 @@ interface HeaderProps {
   rightArea?: boolean;
 }
 
-const Header = ({
-  leftArea = 'home',
-  children,
-  rightArea = true
-}: React.PropsWithChildren<HeaderProps>) => {
-  const navigate = useNavigate();
-  const notification = useUserNotifications();
-  const { user } = useUser();
+const Header = memo(
+  ({
+    leftArea = 'home',
+    children,
+    rightArea = true
+  }: React.PropsWithChildren<HeaderProps>) => {
+    const navigate = useNavigate();
+    const notification = useUserNotifications();
+    const { user } = useUser();
 
-  const newNotification = notification.filter((item) => !item.seen);
+    const newNotification = notification.filter((item) => !item.seen);
 
-  const handleGoBack = () => {
-    navigate(-1);
-  };
+    const handleGoBack = () => {
+      navigate(-1);
+    };
 
-  return (
-    <header className="fixed top-0 z-10 flex h-28 w-screen max-w-[767px] justify-center bg-main-lighten text-white">
-      <h1 className="mt-14 inline-block text-xl">{children}</h1>
-      {leftArea === 'home' ? (
-        <span className="absolute left-6 top-[3.75rem] flex h-6 w-6 items-center justify-start stroke-white text-xl text-white">
-          홈
-        </span>
-      ) : (
-        <Button
-          className="absolute left-6 top-[3.75rem] flex h-6 w-6 items-center justify-start cs:p-0"
-          onClick={handleGoBack}
-        >
-          <LeftArrowIcon className="h-5 w-3 fill-white" />
-        </Button>
-      )}
-      {rightArea && (
-        <div className="absolute right-6 top-[3.75rem] flex gap-4">
-          <Link to="/search">
-            <SearchIcon className="h-6 w-6 stroke-white" />
-          </Link>
-          {user && (
-            <Link to="/notifications">
-              <BellIcon className="h-6 w-6 stroke-white" />
-              {newNotification.length > 0 && (
-                <Badge className="absolute -top-1 right-[6px] flex translate-x-1/2 items-center justify-center border-none text-[10px] text-white cs:bg-active-base cs:px-1 cs:py-0">
-                  {newNotification.length}
-                </Badge>
-              )}
+    return (
+      <header className="fixed top-0 z-10 flex h-28 w-screen max-w-[767px] justify-center bg-main-lighten text-white">
+        <h1 className="mt-14 inline-block text-xl">{children}</h1>
+        {leftArea === 'home' ? (
+          <span className="absolute left-6 top-[3.75rem] flex h-6 w-6 items-center justify-start stroke-white text-xl text-white">
+            홈
+          </span>
+        ) : (
+          <Button
+            className="absolute left-6 top-[3.75rem] flex h-6 w-6 items-center justify-start cs:p-0"
+            onClick={handleGoBack}
+          >
+            <LeftArrowIcon className="h-5 w-3 fill-white" />
+          </Button>
+        )}
+        {rightArea && (
+          <div className="absolute right-6 top-[3.75rem] flex gap-4">
+            <Link to="/search">
+              <SearchIcon className="h-6 w-6 stroke-white" />
             </Link>
-          )}
-        </div>
-      )}
-    </header>
-  );
-};
+            {user && (
+              <Link to="/notifications">
+                <BellIcon className="h-6 w-6 stroke-white" />
+                {newNotification.length > 0 && (
+                  <Badge className="absolute -top-1 right-[6px] flex translate-x-1/2 items-center justify-center border-none text-[10px] text-white cs:bg-active-base cs:px-1 cs:py-0">
+                    {newNotification.length}
+                  </Badge>
+                )}
+              </Link>
+            )}
+          </div>
+        )}
+      </header>
+    );
+  }
+);
 
 export default Header;
--- src/pages/SearchPage/components/UserSearchResult.tsx
+++ src/pages/SearchPage/components/UserSearchResult.tsx
@@ -1,29 +1,33 @@
+import { memo } from 'react';
 import { UserList } from './index';
+import { Exclamation } from '~/components/common/Exclamation';
 import { User } from '~/types';
 
 interface UserSearchResultProps {
   userResults: User[];
 }
 
-const UserSearchResult = ({ userResults }: UserSearchResultProps) => {
+const UserSearchResult = memo(({ userResults }: UserSearchResultProps) => {
   return (
-    <section className="relative h-full w-full overflow-hidden">
-      <section className="relative mb-6 flex h-full flex-col justify-evenly rounded-xl border-[1px] text-xs">
+    <section className="relative h-full w-full overflow-x-hidden">
+      <section className="relative mb-6 flex flex-col justify-evenly overflow-hidden rounded-xl border-[1px] bg-white text-xs">
+        <span className="px-4 py-2 text-sm">사용자</span>
         {userResults.length > 0 ? (
           <>
-            <span className="px-4 py-2 text-sm">사용자</span>
             <section className="flex h-full flex-col overflow-y-auto overflow-x-hidden">
               <UserList userResults={userResults} />
             </section>
           </>
         ) : (
-          <span className="flex h-full w-full items-center justify-center text-base">
-            결과 없음
-          </span>
+          <Exclamation className="mb-9 mt-4">
+            <p className="text-[0.875rem] text-gray-400">
+              검색 결과가 없습니다.
+            </p>
+          </Exclamation>
         )}
       </section>
     </section>
   );
-};
+});
 
 export default UserSearchResult;
