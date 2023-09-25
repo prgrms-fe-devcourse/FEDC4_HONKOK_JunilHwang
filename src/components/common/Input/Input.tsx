8a58e8f73e1bcfe5552c446852db049c8b4ee82a
jkea1 <jinkyng7890@gmail.com>
2023-09-25 23:05:00 +0900

    Revert "feat: 테두리 수정 및 패딩 값 변경"
    This reverts commit 5559307585fa201f8dd1d6d50f54408f99caf498.

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -5,7 +5,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ className, ...props }: InputProps, ref) => {
     const defaults =
-      'rounded-[0.625rem] border-[1.5px] border-gray-600 pb-[0.56rem] pl-[0.87rem] pt-[0.5rem] placeholder:text-gray-200 focus:outline-main-base';
+      'rounded-[0.625rem] border-[1.5px] border-gray-200 pb-[0.56rem] pl-[0.87rem] pt-[0.5rem] placeholder:text-gray-200 focus:outline-main-base';
 
     return (
       <input ref={ref} className={`${defaults} ${className}`} {...props} />
