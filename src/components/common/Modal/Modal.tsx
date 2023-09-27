e05b36da12d92d258e8567e21ed075de058f5748
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-09-27 17:35:24 +0900

    style: 중복 속성 제거

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -28,7 +28,7 @@ const Modal = ({
         className="fixed bottom-0 left-0 right-0 top-0 z-50 bg-black bg-opacity-30"
         onClick={handleClose}
       />
-      <div className="fixed left-1/2 top-1/2 z-50 flex w-3/4 w-fit max-w-[20rem] -translate-x-1/2 -translate-y-1/2 transform flex-col rounded-2xl bg-white p-6 font-OAGothic">
+      <div className="fixed left-1/2 top-1/2 z-50 flex w-3/4 max-w-[20rem] -translate-x-1/2 -translate-y-1/2 transform flex-col rounded-2xl bg-white p-6 font-OAGothic">
         <CloseIcon
           className="y-6 z-50 mb-2 w-6 cursor-pointer self-end"
           onClick={handleClose}
